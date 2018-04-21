a2b51516a77a516893400dd6348169af8d1a19ac
Thu <thu2468@gmail.com>
2018-04-21 00:02:09 -0400

    Added Employee Type

--- src/ProjectApplication.java
+++ src/ProjectApplication.java
@@ -1,8 +1,62 @@
+import javax.swing.JOptionPane;
 
 public class ProjectApplication {
 	public static void main(String[] args) {
-		
-
+		String buildingName;
+		boolean invalid = true;
+		do {
+			buildingName = JOptionPane.showInputDialog("Enter Building Name");
+			if (buildingName == null || buildingName.equals("")) {
+				JOptionPane.showMessageDialog(null, "Building Name cannot be blank");
+			} else {
+				invalid = false;
+			}
+		} while (invalid);
+		int numEmployee = enterNumEmployees();
+		Employee<Integer>[] employees = new Employee[numEmployee]; //has a warning?
+		for(int x = 0; x < numEmployee; x++) {
+			employeeType(employees, x);
+		}
+		//Room[] rooms = new Room[5];
 	}
-
+	
+	public static int enterNumEmployees() {
+		int totalEmployees = 0;
+		boolean invalid = true;
+		try {
+			do {
+				totalEmployees = Integer.parseInt(JOptionPane.showInputDialog("Enter Number of Employees: "));
+				if (totalEmployees <= 0) {
+					JOptionPane.showMessageDialog(null, "Number of Employee must be greater than 0");
+				} else {
+					invalid = false;
+				}
+			} while(invalid);
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Number of Employee Must be a number");
+		}
+		return totalEmployees;
+	}
+	public static void employeeType(Employee<Integer>[] employees,int x) {
+		String[] choices = {"Manager","Construction Worker"};
+		String input = (String) JOptionPane.showInputDialog(null, "Choose type","Choice type of Employee", JOptionPane.QUESTION_MESSAGE, null, choices,choices[0]);
+		switch(input) {
+			case "Manager": 
+				employees[x] = new Manager<Integer>();
+				break;
+			case "Construction Worker":
+				employees[x] = new ConstructionWorker<Integer>();
+				break;
+			default: break;
+		}
+	}
+	public static void employeeRate() {
+		
+	}
+	public static void employeeData() {
+		
+	}
+	public static void roomData() {
+		
+	}
 }
